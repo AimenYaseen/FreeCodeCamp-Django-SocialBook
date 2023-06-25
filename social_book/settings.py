80ee50e789f48face2955552e48ba7b14145bebb
AimenYaseen <aimenyaseen6@gmail.com>
2023-06-25 18:20:31 +0500

    add path to template files

--- social_book/settings.py
+++ social_book/settings.py
@@ -38,7 +38,7 @@ ROOT_URLCONF = 'social_book.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
