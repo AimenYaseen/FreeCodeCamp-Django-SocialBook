1ad7ef6d3a794f06565d0144c624d678e2e8fa60
AimenYaseen <aimenyaseen6@gmail.com>
2023-06-26 22:05:23 +0500

    user profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    user_id = models.IntegerField()
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profile_images', default='default_user_image.png')
+    location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        self.user.username
