a804896bb4a3e1dd7b97f3f8e37d9a6f63b10d50
AimenYaseen <aimenyaseen6@gmail.com>
2023-07-03 10:45:02 +0500

    signup view

--- user_auth/views.py
+++ user_auth/views.py
@@ -6,3 +6,10 @@ def index(request):
     Home page renders index template
     """
     return render(request, 'index.html')
+
+
+def signup(request):
+    """
+    renders signup template
+    """
+    return render(request, 'signup.html')
