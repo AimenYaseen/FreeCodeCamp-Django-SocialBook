c64315214b2bf676bb701050ab8f2fe6ffad8147
AimenYaseen <aimenyaseen6@gmail.com>
2023-07-06 11:39:49 +0500

    check_user will check user with the given keyword arguments

---
+++ user_auth/db_actions.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import BadRequest
+
+from users.models import Profile
+
+
+class UserDbActions:
+    @staticmethod
+    def check_user(field, **kwargs):
+        """
+        check if user exists with the given keyword arguments
+        raise an error if it does not exist
+        """
+        if User.objects.filter(**kwargs).exists():
+            raise BadRequest(f'User with this {field} "{kwargs.get(field)}" already Exists')
