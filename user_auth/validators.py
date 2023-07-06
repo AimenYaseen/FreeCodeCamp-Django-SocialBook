103b42c5fd3459e0a41eb84c004f368e65705523
AimenYaseen <aimenyaseen6@gmail.com>
2023-07-06 11:41:04 +0500

    validate username validator

---
+++ user_auth/validators.py
@@ -0,0 +1,14 @@
+from django.core.exceptions import ValidationError
+
+from user_auth.db_actions import UserDbActions
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.validators import UnicodeUsernameValidator
+
+
+class UserAuthValidators:
+    @staticmethod
+    def validate_username(value):
+        try:
+            UnicodeUsernameValidator(value)
+        except ValidationError as exc:
+            raise ValidationError(str(exc))
