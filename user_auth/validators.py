e23e738a0d18c012d2c0d9b6b2f239e87d968570
AimenYaseen <aimenyaseen6@gmail.com>
2023-07-07 11:15:52 +0500

    added validators doc string

--- user_auth/validators.py
+++ user_auth/validators.py
@@ -8,6 +8,9 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 class UserAuthValidators:
     @staticmethod
     def validate_username(value):
+        """
+        Validate username with django default username validator
+        """
         try:
             UnicodeUsernameValidator(value)
         except ValidationError as exc:
@@ -15,6 +18,9 @@ class UserAuthValidators:
 
     @staticmethod
     def validate_password(value):
+        """
+        Validate password with django default password validator
+        """
         try:
             validate_password(value)
         except ValidationError as exc:
@@ -22,6 +28,9 @@ class UserAuthValidators:
 
     @staticmethod
     def validate_signup_input(data):
+        """
+        Validate Signup incoming data and return the validated data
+        """
         email = data.get('email').strip()
         username = data.get('username').strip()
         password = data.get('password').strip()
