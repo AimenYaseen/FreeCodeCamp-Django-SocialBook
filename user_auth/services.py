06bb9a4589d8e1326a33101ec3aa2b6bd50bcd0c
AimenYaseen <aimenyaseen6@gmail.com>
2023-07-07 11:16:32 +0500

    added user_auth service doc strings

--- user_auth/services.py
+++ user_auth/services.py
@@ -7,6 +7,10 @@ from user_auth.validators import UserAuthValidators
 class UserAuthServices:
     @staticmethod
     def signup_service(request):
+        """
+        This service call validate_signup_input to validate incoming data
+        and create_user if data is valid
+        """
         validated_data = UserAuthValidators.validate_signup_input(request.POST)
         UserDbActions.create_user(validated_data)
         messages.info(request, 'User has created Successfully!')
